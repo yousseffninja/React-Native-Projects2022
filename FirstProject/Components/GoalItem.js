707a0877d286c12b62c2bfde2ee54041a6483493
yousseffninja <yousseffmohamedd22@gmail.com>
2022-05-24 15:23:57 +0200

    My React native projects

---
+++ FirstProject/Components/GoalItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const GoalItem = props => {
+    return <View style={styles.item}>
+        <Text>{props.title}</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
+    },
+})
+
+export default GoalItem
